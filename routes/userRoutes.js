849d3f3e753806fd3ada05db217922a9193d097d
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-29 20:50:47 -0300

    funcao getUsers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,3 +2,16 @@ const fs = require('fs')
 const { join }  = require('path')
 
 const filePath = join(__dirname, 'users.json')
+
+const getUsers = () => {
+    const data = fs.existsSync(filePath)
+        ? fs.readFileSync(filePath)
+        : []
+
+        try{
+            return JSON.parse(data)
+        } catch (error) {
+            return []
+        }
+
+    }
