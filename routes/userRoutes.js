b37631cff7b787f91258ea93840baef8824b8682
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-29 20:55:04 -0300

    Rotas /users delete

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -56,7 +56,13 @@ const userRoute = (app) => {
 
         res.status(200).send('OK');
     })
-    
+    .delete((req, res) => {
+        const users = getUsers()
+
+        saveUser(users.filter(user => user.id !== req.params.id))
+
+        res.status(200).send('OK');
+    })
 }
 
-module.exports = userRoute;
+
