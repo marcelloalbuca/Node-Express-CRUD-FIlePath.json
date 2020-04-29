8bec39f17ed041e21a2201b3087fe907ab81b968
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-29 20:54:42 -0300

    Rotas /users PUT

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -40,7 +40,23 @@ const userRoute = (app) => {
             res.status(201).send('OK')
 
     })
+    .put((req, res) => {
+        const users = getUsers()
+
+        saveUser(users.map(user => {
+            if (user.id === req.params.id) {
+                return {
+                    ...user,
+                    ...req.body
+                }
+            }
+
+            return user
+        }))
+
+        res.status(200).send('OK');
+    })
     
 }
 
-
+module.exports = userRoute;
